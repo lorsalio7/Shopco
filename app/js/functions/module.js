d7203bda91143770cf70b8491c44417ce0f467b1
lorsalio7 <lorsalio7@gmail.com>
2025-09-04 22:11:40 +0500

    Изменить фон в блоке subscribe

--- app/js/functions/module.js
+++ app/js/functions/module.js
@@ -1,6 +1,6 @@
 @@include("parts/debounce.js");
 @@include("parts/throttle.js");
-@@include("parts/zoom-ios.js");
+@@include("parts/disableIosTextFieldZoom.js");
 @@include("parts/isSafari.js");
 @@include("parts/scrollController.js");
 @@include("parts/changeView.js");
